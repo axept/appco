b9f16aaa5d33616bd52f70320f32750e821b644d
Grigory <grigory.leonenko@gmail.com>
2018-12-19 19:20:48 +0300

    Add object support, eslint and travis build. Update babel.

--- src/index.js
+++ src/index.js
@@ -1,132 +1,151 @@
 import fs from 'fs'
+import path from 'path'
 
-/** eslint-disable no-param-reassign */
+export const types = {
+  string: '@appco/string',
+  number: '@appco/number',
+  boolean: '@appco/boolean',
+  array: '@appco/array',
+  object: '@appco/object',
+}
 
 /**
- * @param {Object} config
+ * @param {Object} config Configuration object.
  * @param {Object|String} [profile] Profile data or content from profile file, if this param is
  *                                  not specified - PROFILE env parameter is using
  *                                  by default. You can set it to false to avoid any profile data.
+ * @returns {Object} config
  */
 export function applyProfileToConfig(config, profile) {
-  let profileData;
+  let profileData
   if (typeof profile === 'object') {
-    profileData = profile;
+    profileData = profile
   } else if (typeof profile === 'string') {
-    const finalProfile = profile || process.env['PROFILE'];
-    const profileInJS = path.join(process.cwd(), profile + '.js');
+    const profileInJS = path.join(process.cwd(), `${profile}.js`)
     if (fs.existsSync(profileInJS)) {
-      profileData = require(profileInJS);
+      profileData = require(profileInJS)
     } else {
-      const profileInJSON = path.join(process.cwd(), profile + '.json');
-      const profileContent = fs.readFileSync(profileInJSON);
-      profileData = JSON.parse(profileContent);
+      const profileInJSON = path.join(process.cwd(), `${profile}.json`)
+      const profileContent = fs.readFileSync(profileInJSON)
+      profileData = JSON.parse(profileContent)
     }
   }
 
-  Object.keys(config).map(key => {
-    const value = profileData[key];
-  if (value) {
-    config[key]['value'] = value;
-  }
-});
+  Object.keys(config).forEach((key) => {
+    const value = profileData[key]
+    if (value) {
+      config[key]['value'] = value
+    }
+  })
 
-  return config;
+  return config
 }
 
 export function applyEnvToConfig(config) {
-  Object.keys(config).map(key => {
-    const { env, type } = config[key];
-  let envKey;
-  if (typeof env === 'string') {
-    envKey = env;
-  } else if (env === true) {
-    envKey = key;
-  }
-  const value = env && process.env && process.env[envKey];
-  if (env && value) {
-    switch (type) {
-      case 'string':
-        config[key]['value'] = value;
-        break;
-      case 'number':
-        config[key]['value'] = parseInt(value, 10);
-        break;
-      case 'boolean':
-        config[key]['value'] = value === 'true';
-        break;
-      case 'array':
-        config[key]['value'] = value.split(',');
-        break;
-      default:
-        console.error(`Wrong type ${type} defined for key ${key}`);
+  Object.keys(config).forEach((key) => {
+    const { env, type } = config[key]
+    let envKey
+    if (typeof env === 'string') {
+      envKey = env
+    } else if (env === true) {
+      envKey = key
     }
-  }
-});
+    const value = env && process.env && process.env[envKey]
+    if (env && value) {
+      switch (type) {
+        case types.string:
+          config[key]['value'] = value
+          break
+        case types.number:
+          config[key]['value'] = parseInt(value, 10)
+          break
+        case types.boolean:
+          config[key]['value'] = value === 'true'
+          break
+        case types.array:
+          config[key]['value'] = value.split(',')
+          break
+        case types.object:
+          try {
+            config[key]['value'] = JSON.parse(value)
+          } catch (error) {
+            console.error(`Wrong env value ${value} defined for key ${key}`)
+          }
+          break
+        default:
+          console.error(`Wrong type ${type} defined for key ${key}`)
+      }
+    }
+  })
 
-  return config;
+  return config
 }
 
 export function applyNamespaceToConfig(config, namespaces) {
-  const filter = Object.keys(namespaces).some(name => namespaces[name] === true);
-  Object.keys(config).map(key => {
-    const { namespace } = config[key];
-  if (!namespace) {
-    console.error(`Namespace for key ${key} not defined`);
-    delete config[key];
-  } else if (filter) {
-    const matched = namespace.some(name => namespaces[name]);
-    if (!matched) {
-      delete config[key];
+  const filter = Object.keys(namespaces).some(name => namespaces[name] === true)
+  Object.keys(config).forEach((key) => {
+    const { namespace } = config[key]
+    if (!namespace) {
+      console.error(`Namespace for key ${key} not defined`)
+      delete config[key]
+    } else if (filter) {
+      const matched = namespace.some(name => namespaces[name])
+      if (!matched) {
+        delete config[key]
+      }
     }
-  }
-});
-  return config;
+  })
+  return config
 }
 
 export function validateConfigValues(config) {
-  Object.keys(config).map(key => {
-    const { value, type, required } = config[key];
-  if (required && typeof value === 'undefined') {
-    console.error(`Value for key ${key} must be defined`);
-  } else if (typeof value !== 'undefined') {
-    switch (type) {
-      case 'string':
-        if (typeof value !== 'string') {
-          console.error(`Value for key ${key} must be string`);
-        }
-        break;
-      case 'number':
-        if (isNaN(value) || typeof value !== 'number') {
-          console.error(`Value for key ${key} must be number`);
-        }
-        break;
-      case 'boolean':
-        if (typeof value !== 'boolean') {
-          console.error(`Value for key ${key} must be boolean`);
-        }
-        break;
-      case 'array':
-        if (Object.prototype.toString.call(value) !== '[object Array]') {
-          console.error(`Value for key ${key} must be array`);
-        }
-        break;
-      default:
-        console.error(`Wrong type ${type} defined for key ${key}`);
+  Object.keys(config).forEach((key) => {
+    const {
+      value, type, required, validate = true,
+    } = config[key]
+    if (required && typeof value === 'undefined') {
+      console.error(`Value for key ${key} must be defined`)
+    } else if (typeof value !== 'undefined' && validate) {
+      switch (type) {
+        case types.string:
+          if (typeof value !== 'string') {
+            console.error(`Value for key ${key} must be string`)
+          }
+          break
+        case types.number:
+          if (Number.isNaN(value) || typeof value !== 'number') {
+            console.error(`Value for key ${key} must be number`)
+          }
+          break
+        case types.boolean:
+          if (typeof value !== 'boolean') {
+            console.error(`Value for key ${key} must be boolean`)
+          }
+          break
+        case types.array:
+          if (Object.prototype.toString.call(value) !== '[object Array]') {
+            console.error(`Value for key ${key} must be array`)
+          }
+          break
+        case types.object:
+          if (Object.prototype.toString.call(value) !== '[object Object]') {
+            console.error(`Value for key ${key} must be plain object`)
+          }
+          break
+        default:
+          console.error(`Wrong type ${type} defined for key ${key}`)
+      }
     }
-  }
-});
+  })
 
-  return config;
+  return config
 }
 
 export function transformConfigToValues(config) {
-  Object.keys(config).map(key => {
-    const { value } = config[key];
-  config[key] = value;
-});
+  Object.keys(config).forEach((key) => {
+    const { value } = config[key]
+    config[key] = value
+  })
 
-  return config;
+  return config
 }
-
-/** eslint-enable no-param-reassign */
